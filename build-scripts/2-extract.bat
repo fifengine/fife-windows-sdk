3f614a25983b66b08dca9f1526956fbdf8190373
jakoch <jakoch@web.de>
2016-11-15 15:00:57 +0100

    added extract script (downloads -> extracted)

---
+++ build-scripts/2-extract.bat
@@ -0,0 +1,39 @@
+@echo off
+
+set ZIP="..\build-tools\7zip\x64\7za.exe"
+set TARGET_DIR="..\extracted"
+
+pushd downloads
+
+rem dir
+
+if not exist %TARGET_DIR% (mkdir %TARGET_DIR%)
+
+echo.
+echo Extracting files into the folder %TARGET_DIR%
+
+:: Extract - Build Tools
+
+echo.
+echo CMake
+%ZIP% x cmake.zip -o%TARGET_DIR% > nul
+move "%TARGET_DIR%\cmake-*" "%TARGET_DIR%\cmake"
+
+echo.
+echo Swig
+%ZIP% x swig.zip -o%TARGET_DIR% > nul
+move "%TARGET_DIR%\swig*" "%TARGET_DIR%\swig"
+
+rem echo.
+rem echo Python 
+rem %ZIP% x ActivePython.zip -o%TARGET_DIR% > nul
+rem move "%TARGET_DIR%\ActivePython-*" "%TARGET_DIR%\python-installer"
+
+rem echo.
+rem echo TDM-GCC-MINGW
+rem %ZIP% x tdm-gcc.7z -o%TARGET_DIR% > nul
+rem move "%TARGET_DIR%\tdm-gcc-*" "%TARGET_DIR%\mingw"
+
+:: Extract - Dependencies of Fifengine
+
+popd
